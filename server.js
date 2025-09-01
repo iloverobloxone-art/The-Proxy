22e196c8111ce33d7fd0f7ed3012b5d4b4e086d4
iloverobloxone-art <iloveroblox.one@gmail.com>
2025-09-01 17:56:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const app = express();
-app.use('/proxy', createProxyMiddleware({
+const app = express();app.use('/proxy', createProxyMiddleware({
   target: '', // set dynamically
   changeOrigin: true,
   router: req => req.query.url,
   pathRewrite: { '^/proxy': '' }
+}));
+
 }));
 app.get('/', (req, res) => res.send('Proxy server running!'));
 app.listen(process.env.PORT || 3000);
